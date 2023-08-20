8314b3e675d433f8fc1521f98f671a89a15fa02a
Noah Alemayehu <noahlem21@gmail.com>
2023-08-20 11:17:24 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            // Handle the conversion specifiers
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    while (*str != '\0')
+                    {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%':
+                {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
